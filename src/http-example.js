0efedcf35f4f8cf4b507a8b9d4f5ed53f2b768dc
Michael Sprague <mikesprague@gmail.com>
2025-02-26 09:05:00 -0500

    refactor(http-example): use fetch instead of axios

--- src/http-example.js
+++ src/http-example.js
@@ -1,5 +1,4 @@
 import { app } from '@azure/functions';
-import axios from 'axios';
 
 import { handleError } from '../lib/helpers.js';
 
@@ -7,21 +6,21 @@ app.http('http-example', {
   methods: ['GET'],
   handler: async (request, context) => {
     try {
-      const joke = await axios
-        .get('https://icanhazdadjoke.com/', {
-          headers: {
-            Accept: 'application/json',
-            'User-Agent': 'Azure Function App - HTTP Endpoint Demo',
-          },
-        })
-        .then((response) => {
-          const { joke } = response.data;
+      const joke = await fetch('https://icanhazdadjoke.com/', {
+        headers: {
+          Accept: 'application/json',
+          'User-Agent': 'Azure Function App - HTTP Endpoint Demo',
+        },
+      })
+        .then(async (response) => {
+          const { joke } = await response.json();
           return joke;
         })
         .catch((error) => {
           // bubble error up to try/catch so it throws 500
           throw error;
         });
+
       return {
         status: 200,
         body: JSON.stringify({
